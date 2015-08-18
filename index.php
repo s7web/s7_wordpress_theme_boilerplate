516d82cc34de601954df44c9ff10976335b89470
s7npaic <npaic@s7designcreative.com>
2015-08-18 17:30:57 +0200

    Added init of index

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * Template : Default template for theme
+ *
+ * @package S7design
+ */
+
+
+if ( have_posts() ) :
+	while ( have_posts() ):
+		the_post();
+		?>
+		<article>
+			<header>
+				<h1><?php the_title(); ?></h1>
+			</header>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+		<?php
+	endwhile;
+endif;
