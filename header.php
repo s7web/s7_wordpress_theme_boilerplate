1532e7858064f618aafd1c57291ce52cdafa373c
s7npaic <npaic@s7designcreative.com>
2015-08-18 17:36:56 +0200

    Added base content in header

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template : Header
+ *
+ * @package S7design
+ */
+?>
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+	<!--=== META TAGS ===-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="author" content="Name">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!--=== LINK TAGS ===-->
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<!--=== TITLE ===-->
+	<title><?php bloginfo( 'name' ); ?><?php wp_title( '-' ); ?> </title>
+
+	<!--=== WP_HEAD() ===-->
+	<?php wp_head(); ?>
+</head>
+<body>
