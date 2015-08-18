d1e8c1bce33d8c6e3313751cf0b75350f7c6a839
s7npaic <npaic@s7designcreative.com>
2015-08-18 17:17:16 +0200

    init of functions in theme

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Functions for theme
+ *
+ * @package S7design
+ */
+add_action( 'after_setup_theme', 's7design_theme_setup' );
+
+/**
+ * Set up theme
+ *
+ * @return void
+ */
+function s7design_theme_setup() {
+
+
+}
