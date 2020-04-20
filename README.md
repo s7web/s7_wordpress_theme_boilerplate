1be41ff94e443cdcf846b3eb29d867c43c3ce5a0
root <office@s7designcreative.com>
2020-04-20 20:20:15 +0200

    Updated WP boilerplate with Bootstrap 4.4.1 support

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # s7_wordpress_theme_boilerplate
 S7 design wordpress theme boilerplate
+
+Bootstrap 4.4.1
+Popper.js 1.16.0
+
+#Nav Walekr menu
+
+wp_nav_menu( array(
+    'theme_location'  => 'primary',
+    'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+    'container'       => 'div',
+    'container_class' => 'collapse navbar-collapse',
+    'container_id'    => 'bs-example-navbar-collapse-1',
+    'menu_class'      => 'navbar-nav mr-auto',
+    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+    'walker'          => new WP_Bootstrap_Navwalker(),
+) ); -->
--- functions.php
+++ functions.php
@@ -15,3 +15,74 @@ function s7design_theme_setup() {
 
 
 }
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+
+//funtion for registering CSS files included bootstrap 4.4
+
+function load_css(){
+
+    //register styles
+    wp_register_style( 'bootstrapmin', get_template_directory_uri() . 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all' );
+    wp_register_style( 'customcss', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0', 'all' );
+        //wp_register_style( 'bootstraptheme', get_template_directory_uri() . '/css/bootstrap-theme.min.css', array(), '3.3.7', 'all' );
+    
+    //enqueue registerd styles
+    wp_enqueue_style( 'bootstrapmin' );
+    wp_enqueue_style( 'customcss' );
+}
+
+add_action('wp_enqueue_scripts', 'load_css');
+
+//function for registering JS
+
+function load_js(){
+
+    //enqueue jquery script before all
+    wp_enqueue_script( 'jquery');
+
+    //register script
+    wp_register_script( 'popperjs', get_template_directory_uri() . 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', 'jquery', '1.16.0', true );
+    wp_register_script( 'bootstrapjs', get_template_directory_uri() . 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), false, true );
+    wp_register_script( 'customjs', get_template_directory_uri() . get_template_directory_uri() . '/js/custom.js', array(), false, true );
+
+    //enqueue registered scripts
+    wp_enqueue_script( 'popperjs');
+    wp_enqueue_script( 'bootstrapjs');
+    wp_enqueue_script( 'customjs');
+}
+
+add_action('wp_enqueue_scripts', 'load_js');
+
+//Register menu placeholder
+
+register_nav_menus( 
+    
+    array(
+
+        'top-menu-front-page' => "Top Menu front location",
+        'top-menu-global-page' => "Top Menu global location",
+
+    )
+
+);
+
+//Add theme support
+
+//add theme support for display Menu
+add_theme_support( 'menus');
+//add theme support for title tag
+add_theme_support( 'title-tag' );
+//add theme support for post thumbnails
+add_theme_support( 'post-thumbnails');
+//add theme support for widgets
+add_theme_support( 'widgets');
+//add theme support for Yoast plugin
+add_theme_support( 'yoast-seo-breadcrumbs' );
